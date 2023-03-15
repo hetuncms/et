d8fb5db964cfb222c57db72e7f977fa136736bb8
test <test@qq.com>
2023-03-15 15:01:46 +0800

    解决两个问题 1. 视频播放黑屏 小窗播放正常 2. banner_ad 的模板里获取不到config对象

--- src/main/java/com/example/cms9cc/template/PlayController.java
+++ src/main/java/com/example/cms9cc/template/PlayController.java
@@ -1,5 +1,6 @@
 package com.example.cms9cc.template;
 
+import com.example.cms9cc.admin.AdminService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,13 +12,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/live")
 public class PlayController {
 
+    AdminService adminService;
+
+    public PlayController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @GetMapping("/basketball/{id}")
     public String playBasketBall(HttpServletRequest request, Model model, @PathVariable("id") Integer id){
         model.addAttribute("requestUrl", request.getRequestURI());
+        getBaseModel(model);
         return "bofang";
     }
 
     public String playFootBall(@PathVariable Integer id){
         return "bofang";
+    }
+
+    public Model getBaseModel(Model model) {
+
+        model.addAttribute("config", adminService.getAllConfig().getBannerAd());
+        return model;
     }
 }
