629d3068fce337e749bbc19cf1d8f5f7bff9b977
test <test@qq.com>
2023-03-18 15:09:42 +0800

    video样式

--- src/main/java/com/example/cms9cc/admin/bean/FloatBean.java
+++ src/main/java/com/example/cms9cc/admin/bean/FloatBean.java
@@ -1,9 +1,15 @@
 package com.example.cms9cc.admin.bean;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
 
 @Entity
 @Table(name="float_ad")
+@Setter
+@Getter
 public class FloatBean {
     @Id
     @Column(name = "id", nullable = false)
@@ -11,57 +17,9 @@ public class FloatBean {
 
     private String url;
     private String picUrl;
-    private String status;
+    private Date status;
     private String tel;
     @Transient
     private boolean update;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public boolean isUpdate() {
-        return update;
-    }
-
-    public void setUpdate(boolean update) {
-        this.update = update;
-    }
-
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getPicUrl() {
-        return picUrl;
-    }
-
-    public void setPicUrl(String picUrl) {
-        this.picUrl = picUrl;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
 }
